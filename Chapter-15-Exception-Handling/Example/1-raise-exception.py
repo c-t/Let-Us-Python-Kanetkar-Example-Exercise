1435d67a45d4676e7fbd9d384c682f9bb686e675
Christ Toppo <christ.toppo@gmail.com>
2020-09-26 13:06:50 +0530

    Add files via upload

---
+++ Chapter-15-Exception-Handling/Example/1-raise-exception.py
@@ -0,0 +1,5 @@
+"""
+WAP to infinitely receives positive integer as input and prints its square.
+If a negative number is entered then raise an exception, display relevant error message and
+make a graceful exit.
+"""
